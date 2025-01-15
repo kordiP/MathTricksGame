eeb120751c1efd12281744ea720e12fb2bdba403
Atanas Kordov <nasikordov@gmail.com>
2025-01-15 06:43:23 +0200

    Almost finished grid reading from file, made saving to file possible + added neccessary comments

--- MathTricks/Constants.h
+++ MathTricks/Constants.h
@@ -1,13 +1,24 @@
 #pragma once
 
-const int FieldSizeMin = 4;
-const int FieldSizeMax = 14;
+const int GRID_SIZE_MIN = 4;
+const int GRID_SIZE_MAX = 14;
 
-const int OperationsCount = 4;
-const char OperationSymbols[] = { '+', '-', '*', '/' };
+const int OPERATIONS_COUNT = 4;
+const char OPERATIONS_ARR[] = { '+', '-', '*', '/' };
 
-const int ColorBlack = 0;
-const int ColorBlue = 9;
-const int ColorGreen = 10;
-const int ColorYellow = 14;
-const int ColorWhite = 15;
+const int COLOR_BLACK = 0;
+const int COLOR_BLUE = 9;
+const int COLOR_GREEN = 10;
+const int COLOR_YELLOW = 14;
+const int COLOR_WHITE = 15;
+
+const int PLAYER_A_COLOR = COLOR_YELLOW + 16 * COLOR_BLUE;
+const char PLAYER_A_NAME[] = "Player 1 (blue)";
+
+const int PLAYER_B_COLOR = COLOR_YELLOW + 16 * COLOR_GREEN;
+const char PLAYER_B_NAME[] = "Player 2 (Green)";
+
+const int CELL_VAL_RANGE = 2;
+const int MULTIPLIER_MAX = 4;
+
+const int FILE_LINE_LEN_MAX = 255;
--- MathTricks/HelperFunctions.h
+++ MathTricks/HelperFunctions.h
@@ -5,4 +5,5 @@ void clearConsole();
 int maxNum(int num1, int num2);
 int minNum(int num1, int num2);
 int absNum(int num);
+int ceilNum(double num);
 void stringCopy(char* destination, const char* source);
